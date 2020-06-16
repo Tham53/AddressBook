252152f245fdd7ef611a97b235c189c5330f63ea
Tham53 <59705897+Tham53@users.noreply.github.com>
2020-06-16 14:41:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,29 @@
-window.onload = function() {
-  getMultiple();
-}
-//grabbing persons information 
-let arrayOfPersons = [];
-const getPerson = () => {
+let postArray = [];
+let userArray = []
+const retrieveSinglePosts = () =>{
+  let newPost;
   fetch('https://randomuser.me/api/')
-  .then(res => res.json())
-  .then(user => user.results.map(person => {
-    arrayOfPersons.push(person)
-    console.log(person)
-    const list = document.getElementById("allUsers")
-    //create li
-    const pic = document.createElement("img")
-    pic.setAttribute('src', '${person.picture.thumbnail}')
-    const li = document.createElement("li")
-    const text = document.createTextNode('src', '${person.name.first}')
-    const button = document.createElement('button')
-    //button info
-    //adding phone email dob line break
-    const Infobutton = document.createElement("More Info")
-    const liInfo = document.createElement("li")
-    const textInfo = document.createTextNode('${person.phone}, ${person.emergency}')
-    const lineBreak = document.createElement('br')
-    li.appendChild(pic)
-    li.appendChild(text)
-    list.appendChild(li)
-    list.appendChild(button)
-    button.appendChild(Infobutton)
-    liInfo.appendChild(textInfo)
-    button.addEventListener("click", function(){
-      const textInfo = document.createTextNode('${person.phone}, ${person')
-      li.appendChild(lineBreak)
-      li.appendChild(textInfo)})
-    }));
-  }
-
-
-
-  const getMultiple = () => {
-    fetch('https://randomuser.me/api/?results=5')
     .then(res => res.json())
-    .then(user => user.results.map(person => {
-      arrayOfPersons.push(person)
-      console.log(person)
-      const list = document.getElementById('allUsers')
-      const pic = document.createElement("img")
-    }))
-  }
+    .then(posts => userArray.push(posts))   
+}
+const consolePostsList = () => {
+  console.log(userArray)
+}
+const displayThePost = () => {
+  const allPosts= document.getElementById('all-posts')
+  userArray.map((post, index) => {
+    const li = document.createElement('li')
+    const text = document.createTextNode(`Number${index}, Name: ${post.name.first}:  ${post.body}, by user: ${post.userId}`)
+    li.appendChild(text)
+    allPosts.append(li)
+  })
+}
+const retrieveTheUser = () => {
+  fetch('https://randomuser.me/api/?results=5')
+  .then(res => res.json())
+  .then(posts => postArray = posts)
+}
+
+const displayTheUsers = () =>{
+  console.log(postArray);
+}
