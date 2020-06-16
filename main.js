0537e5ef50328114c0dcee85232f59ebc8428de4
Tham53 <59705897+Tham53@users.noreply.github.com>
2020-06-16 14:16:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,49 @@
-
-
-
-let arrayOfAddress = [];
-
 window.onload = function() {
-  let fetchNum =1;
-  for(let i=0; i<fetchNum; i++){
-    getAddresses()
-  }
+  getMultiple();
 }
-
-const getAddresses = () => {
+//grabbing persons information 
+let arrayOfPersons = [];
+const getPerson = () => {
   fetch('https://randomuser.me/api/')
   .then(res => res.json())
-  .then(addresses => arrayOfAddress.push(addresses))
-  .catch((error) => {
-    console.error('Error: ', error);
-  });
+  .then(user => user.results.map(person => {
+    arrayOfPersons.push(person)
+    console.log(person)
+    const list = document.getElementById("allUsers")
+    //create li
+    const pic = document.createElement("img")
+    pic.setAttribute('src', '${person.picture.thumbnail}')
+    const li = document.createElement("li")
+    const text = document.createTextNode('src', '${person.name.first}')
+    const button = document.createElement('button')
+    //button info
+    //adding phone email dob line break
+    const Infobutton = document.createElement("More Info")
+    const liInfo = document.createElement("li")
+    const textInfo = document.createTextNode('${person.phone}, ${person.emergency}')
+    const lineBreak = document.createElement('br')
+    li.appendChild(pic)
+    li.appendChild(text)
+    list.appendChild(li)
+    list.appendChild(button)
+    button.appendChild(Infobutton)
+    liInfo.appendChild(textInfo)
+    button.addEventListener("click", function(){
+      const textInfo = document.createTextNode('${person.phone}, ${person')
+      li.appendChild(lineBreak)
+      li.appendChild(textInfo)})
+    }));
+  }
 
-  arrayOfAddress.forEach(x => {
-    x.results.name.first
-    console.log(arrayOfAddress)
-    console.log(results.picture.thumbnail)
-  })
-}
 
 
-//console.log
+  const getMultiple = () => {
+    fetch('https://randomuser.me/api/?results=5')
+    .then(res => res.json())
+    .then(user => user.results.map(person => {
+      arrayOfPersons.push(person)
+      console.log(person)
+      const list = document.getElementById('allUsers')
+      const pic = document.createElement("img")
+    }))
+  }
